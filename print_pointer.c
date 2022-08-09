07089b604426e5a7fafda50fa9f07ecc4ecd5e3d
FrankRight <frankbosire2017@gmail.com>
2022-08-09 23:02:15 +0300

    fix parse strings

--- print_pointer.c
+++ print_pointer.c
@@ -1,50 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-
-int _strcmp(char *, char *);
 
 /**
- * print_pointer - Print a number in hexadecimal format
- * @list: Number to print
- *
- * Return: Length of the number
- **/
-int print_pointer(va_list list)
+ * parse_str - writes the string
+ * @buff_dest: character string
+ * @arg: list of arguments
+ * @buff_count: index f buffer pointer
+ * Return: The number of characters printed.
+ */
+int parse_str(char *buff_dest, va_list arg, int buff_count)
 {
-	char *p_buff;
-	int size;
+	char *str;
+	int i = 0;
 
-	p_buff = itoa(va_arg(list, unsigned long int), 16);
+	str = va_arg(arg, char *);
+	if (str == NULL)
+		str = "(null)";
 
-	if (!_strcmp(p_buff, "0"))
-		return (print("(nil)"));
-
-	size = print("0x");
-
-	if (!_strcmp(p_buff, "-1"))
-		size += print("ffffffffffffffff");
-	else
-		size += print(p_buff);
-
-	return (size);
-}
-
-/**
- * _strcmp - Compare two strings
- * @s1: String 1
- * @s2: String 2
- * Return: Integer
- **/
-int _strcmp(char *s1, char *s2)
-{
-	int i;
-
-	for (i = 0; s1[i] != '\0'; i++)
+	if (str[0] == '\0')
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		buff_dest[buff_count] = '\0';
+		buff_count++;
 	}
 
-	return (0);
-}
+	while (str[i] != '\0')
+	{
+		buff_dest[buff_count] = str[i];
+		i++;
+		buff_count++;
+	}
 
+	return (buff_count);
+}
